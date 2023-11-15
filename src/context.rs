c9a56211cf8ad90b7d90fdfb2687254ac9bcc14a
GreenChild04 <greenchild04@protonmail.com>
2023-11-15 11:39:13 +1100

    Added proper logging to motor

--- src/context.rs
+++ src/context.rs
@@ -1,13 +1,26 @@
 use vex_rt::prelude::Peripherals;
+use crate::log::{Logger, Log};
 
 pub struct Context {
     perph: Peripherals,
+    logger: Logger,
 }
 
 impl Context {
     pub fn new(perph: Peripherals) -> Self {
         Self {
             perph,
+            logger: Logger::new(),
         }
+    }
+
+    #[inline]
+    pub fn log(&mut self, log: Log) {
+        self.logger.push(log);
+    }
+
+    #[inline]
+    pub fn flush_logs(&mut self) {
+        todo!();
     }
 }
--- src/motor.rs
+++ src/motor.rs
@@ -1,22 +1,27 @@
-use vex_rt::motor::{EncoderUnits, Gearset, Motor as VexMotor, MotorError};
+use vex_rt::{motor::{EncoderUnits, Gearset, Motor as VexMotor, MotorError}, rtos::Mutex};
+use crate::{context::Context, log::Log};
 
-pub struct Motor {
+pub struct Motor<'a> {
+    context: &'a Mutex<Context>,
     motor: Option<VexMotor>,
     port: u8,
     gear_ratio: Gearset,
     unit: EncoderUnits,
     reverse: bool,
+    logged_disconnect: bool,
 }
 
-impl Motor {
+impl<'a> Motor<'a> {
     #[inline]
-    pub fn build_motor(port: u8, gear_ratio: Gearset, unit: EncoderUnits, reverse: bool) -> Self {
+    pub fn build_motor(context: &'a Mutex<Context>, port: u8, gear_ratio: Gearset, unit: EncoderUnits, reverse: bool) -> Self {
         let mut this = Self {
+            context,
             port,
             gear_ratio,
             unit,
             reverse,
             motor: None,
+            logged_disconnect: false,
         };
 
         this.build_inner_motor();
@@ -25,9 +30,11 @@ impl Motor {
 
     #[inline]
     fn build_inner_motor(&mut self) {
-        if let None = self.motor {
+        if self.motor.is_none() {
             if let Ok(x) = unsafe { VexMotor::new(self.port, self.gear_ratio, self.unit, self.reverse) } {
                 self.motor = Some(x);
+                self.context.lock().log(Log::MotorConnect(self.port));
+                self.logged_disconnect = false;
             }
         }
     }
@@ -35,17 +42,23 @@ impl Motor {
     #[inline]
     pub fn move_voltage(&mut self, voltage: i32) {
         if let Some(x) = &mut self.motor {
-            if let Err(_) = x.move_voltage(voltage) {
-                // error handling here
+            if x.move_voltage(voltage).is_err() && !self.logged_disconnect {
+                self.logged_disconnect = true;
+                self.context.lock().log(Log::MotorError(self.port));
+            } else if self.logged_disconnect {
+                self.context.lock().log(Log::MotorConnect(self.port));
             }
         } else { self.build_inner_motor() }
     }
 
     #[inline]
-    pub fn get_motor(&mut self, f: impl Fn(&mut VexMotor) -> Result<(), MotorError>) {
+    pub fn get_motor(&mut self, f: &'static impl Fn(&mut VexMotor) -> Result<(), MotorError>) {
         if let Some(x) = &mut self.motor {
-            if let Err(_) = f(x) {
-                // error handling here
+            if f(x).is_err() && !self.logged_disconnect {
+                self.logged_disconnect = true;
+                self.context.lock().log(Log::MotorError(self.port));
+            } else if self.logged_disconnect {
+                self.context.lock().log(Log::MotorConnect(self.port));
             }
         } else { self.build_inner_motor() }
     }
