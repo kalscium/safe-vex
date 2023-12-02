99fd4c81c6f39194ab5611880f83deaaf8953053
GreenChild04 <greenchild04@protonmail.com>
2023-12-03 09:17:28 +1100

    Fixed logging timing

--- src/context.rs
+++ src/context.rs
@@ -1,6 +1,6 @@
 use alloc::string::ToString;
 use vex_rt::{prelude::Peripherals, io::println, rtos::Mutex};
-use crate::{log::{Logger, Log}, controller::Controller, colour_format};
+use crate::{log::{Logger, Log}, controller::Controller, colour_format, bot::TICK_SPEED};
 
 pub type TickType = u16;
 
@@ -37,7 +37,7 @@ impl Context {
         let mut tick = 0u32;
         println!("{}", colour_format![blue("\n==="), cyan(" Context  Robot Logs "), blue("===")]);
         self.logger.lock().flush(|log, i| {
-            let tick_str = colour_format![blue("( "), yellow(&(tick as f64/1000f64).to_string()), yellow("s"), blue(" ) ")];
+            let tick_str = colour_format![blue("( "), yellow(&(tick as f64 * TICK_SPEED as f64 /1000f64).to_string()), yellow("s"), blue(" ) ")];
             println!("{}", match log {
                 Log::Other(x) => colour_format![raw(&tick_str), cyan("Custom Log"), blue(": "), none(x)],
                 Log::Autonomous => colour_format![raw(&tick_str), cyan("Autonomous period "), green("started")],
@@ -47,7 +47,7 @@ impl Context {
                 Log::ControllerConnect => colour_format![raw(&tick_str), cyan("Controller "), green("connected")],
                 Log::MotorDisconnect(port) => colour_format![raw(&tick_str), cyan("Motor disconnected "), blue("at "), cyan("port "), yellow(&port.to_string())],
                 Log::MotorConnect(port) => colour_format![raw(&tick_str), cyan("Motor connected "), blue("at "), cyan("port "), yellow(&port.to_string())],
-                Log::Nothing => return,
+                Log::Nothing => return tick += i as u32,
             });
             tick += i as u32;
         });
