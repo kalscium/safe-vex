63a962f89e408a9a74f5592228862e5e1201d31d
GreenChild04 <greenchild04@protonmail.com>
2024-05-07 17:00:21 +1000

    maybe: added maybe struct for use with ports

---
+++ src/maybe.rs
@@ -0,0 +1,31 @@
+/// A struct that may or may not exist (like `Option`)
+///
+/// On an attempt to get the value, it either returns it (Some) or returns None and tries to build it again
+pub struct Maybe<T> {
+    /// internal option
+    value: Option<T>,
+    /// a function that tries to build / create the value
+    build: fn() -> Option<T>,
+}
+
+impl<T> Maybe<T> {
+    /// Creates a new optional version of `T`
+    #[inline]
+    pub fn new(f: fn() -> Option<T>) -> Self {
+        Self {
+            value: f(),
+            build: f,
+        }
+    }
+
+    /// Tries to get the internal value, it either returns it (Some) or returns None and tries to build it again
+    #[inline]
+    pub fn get(&mut self) -> Option<&mut T> {
+        if let Some(ref mut x) = self.value {
+            return Some(x);
+        };
+
+        self.value = (self.build)();
+        self.value.as_mut()
+    }
+}
